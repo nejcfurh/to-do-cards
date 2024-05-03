c447c0d3cecbaa8764737b1737590b46d1cae051
NF-7 <nejc.furh7@gmail.com>
2024-05-03 15:39:56 +0200

    edited folder structure

--- api/api/index.js
+++ api/api/index.js
@@ -6,7 +6,7 @@
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
 import dotenv from 'dotenv';
-dotenv.config({ path: '../.env.local' });
+dotenv.config({ path: './.env.local' });
 import express from 'express';
 import bodyParser from 'body-parser';
 import mongoose from 'mongoose';
@@ -17,8 +17,10 @@ import { expressjwt } from 'express-jwt';
 
 const app = express();
 app.use(cors());
-app.use(express.urlencoded({ extended: true }));
-app.use(express.json());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
 const JWT_TOKEN = process.env.JWT_TOKEN;

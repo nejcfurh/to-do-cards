ec6dcf8300fad5f026d76845ae9c6a58c5c7591a
NF-7 <nejc.furh7@gmail.com>
2024-05-12 16:21:07 +0200

    fixed server

--- index.js
+++ index.js
@@ -350,8 +350,8 @@ app.get('/api/account', requireAuth, async (req, res) => {
 
 // START THE SERVER
 
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+// app.listen(3000, () => {
+//   console.log('Server started on port 3000');
+// });
 
 export default app;
